b4c0cc5f24bae313cc9233383bdcfb8677a07929
jingyanhui <yanhuijing@sf-express.com>
2019-11-13 16:26:53 +0800

    添加fastJson基本用法

---
+++ src/main/java/fastJsonAction/Grade.java
@@ -0,0 +1,54 @@
+package fastJsonAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author : gavin
+ * @Date : 2019-11-1315:15
+ * @DESC : 班级
+ */
+public class Grade {
+
+    private Long id;
+    private String name;
+
+    private List<Student> students = new ArrayList<Student>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", students=" + students +
+                '}';
+    }
+}
